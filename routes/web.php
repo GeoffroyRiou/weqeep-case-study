8bd5c3c748676470ccbd453f9c2cf301568be4ed
Geoffroy Riou <geoffroy@powermedicalsupported.com>
2025-06-13 14:45:57 +0200

    update: deleting the useless test route

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,3 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
-
-Route::get('/test', function () {
-    $api = app(ElectricityMapsService::class);
-
-    try {
-        $result = $api->getPowerBreakdown('FR');
-    } catch (Exception $e) {
-        dd('Error fetching data: '.$e->getMessage());
-    }
-    dd($result);
-})->name('test');
